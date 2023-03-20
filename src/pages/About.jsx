ef50a9b41fd2c7d9290c31234d491fad97007c00
codebuller <ivan_1337_qwerty_339@mail.ru>
2023-03-20 19:55:05 +0300

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MyButton from "../UI/button/MyButton";
+
+const About = () => {
+    return (
+        <div>
+        <h1>Это приложение создано для написания логики клиентской логики</h1>
+        <h1>Без адаптива и дизайна</h1>
+        </div>
+    )
+
+
+}
+export default About;
